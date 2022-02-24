9568cad56dc8d337e481cf44ac73b75c7ac08a36
Santiago Villarreal <fersanty@santys-mbp.home>
2022-02-25 00:48:32 +0100

    File update Multipages Vue Configuration  Changes to be committed: 	modified:   package-lock.json 	deleted:    public/license.html 	modified:   src/components/BodyMap.vue 	modified:   vue.config.js  Untracked files: 	.gitignore

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,34 @@
 module.exports = {
-  publicPath: process.env.RENDER_PATH == undefined
+  /*publicPath: process.env.RENDER_PATH == undefined
     ? '/GEStory/'
     : '/',
-  // outputDir: "docs"
+  // outputDir: "docs"*/
+
+  pages:{
+    index:{
+        entry: "./src/main.js",
+        template: "./public/index.html",
+        filename: "index.html",
+        title:"GEStory"
+    },
+    license:{
+      entry: "./src/pages/license/license.js",
+      template: "./src/pages/license/license.html",
+      filename: "license.html",
+      title:"License"
+  },
+  suggest:{
+    entry: "./src/pages/suggest/suggest.js",
+    template: "./src/pages/suggest/suggest.html",
+    filename: "suggest.html",
+    title:"Suggest a GES"
+  },
+  adminGEStory:{
+    entry: "./src/pages/adminGEStory/adminGEStory.js",
+    template: "./src/pages/adminGEStory/adminGEStory.html",
+    filename: "adminGEStory.html",
+    title:"Manage GEStory"
+  }
+
+  }
 };
